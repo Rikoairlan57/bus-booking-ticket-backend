2a1447d0c06399399c7ef22a1bfa2be1866e5672
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-07 18:19:40 +0700

    create update bus in api

--- routes/busesRoute.js
+++ routes/busesRoute.js
@@ -22,4 +22,16 @@ router.post("/add-bus", authMiddleware, async (req, res) => {
   }
 });
 
+router.post("/update-bus", authMiddleware, async (req, res) => {
+  try {
+    await Bus.findByIdAndUpdate(req.body._id, req.body);
+    return res.status(200).send({
+      success: true,
+      message: "Bus updated successfully",
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+});
+
 module.exports = router;
