9170f726781c7f614c440c8d63ff1b1c4b6104d7
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-07 18:13:12 +0700

    add bus api

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const db = require("./config/db");
 const port = process.env.PORT || 4000;
 app.use(express.json());
 const usersRoute = require("./routes/usersRoute");
+const busesRoute = require("./routes/busesRoute");
 
 app.use("/api/users", usersRoute);
+app.use("/api/buses", busesRoute);
 app.listen(port, () => console.log(`Server run on port ${port}`));
